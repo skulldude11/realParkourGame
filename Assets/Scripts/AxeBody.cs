7f76c3c7cb1f62b25cbd36e4d882c7450595086f
skulldude11 <41588958+skulldude11@users.noreply.github.com>
2020-04-28 20:51:15 -0400

    a game about parkour and fun
    :100:

---
+++ Assets/Scripts/AxeBody.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxeBody : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.C))
+        {
+            transform.localScale = new Vector3( 10.0f, 10.0f, 15.0f);
+        }
+
+        else
+        {
+            transform.localScale = new Vector3(10.0f, 10.0f, 10.0f);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public Animator animator;
+
+
+
+    public void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            animator.SetTrigger("IsAttacking");
+        }
+    }
+}
